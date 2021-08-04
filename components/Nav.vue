2dc90f612b6d87b2c1bba5c470e40653a0d26bc4
polunwu <polunwu@gmail.com>
2021-08-04 18:50:13 +0800

    Remove nav outer div

--- components/Nav.vue
+++ components/Nav.vue
@@ -1,22 +1,20 @@
 <template>
-  <div>
-    <nav id="nav">
-      <NuxtLink to="/characters" class="char-link">
-        <img src="~/assets/images/icons/awesome-book.svg" alt="icon book" />
-      </NuxtLink>
-      <NuxtLink to="/" class="home-link">
-        <img src="~/assets/images/icons/home.svg" alt="icon home" />
-      </NuxtLink>
-      <div
-        class="menu-toggle-btn"
-        :class="{ 'is-open': isNavOpen }"
-        @click="toggleNav"
-      >
-        <span class="bar top-bar"></span>
-        <span class="bar bottom-bar"></span>
-      </div>
-    </nav>
-  </div>
+  <nav id="nav">
+    <NuxtLink to="/characters" class="char-link">
+      <img src="~/assets/images/icons/awesome-book.svg" alt="icon book" />
+    </NuxtLink>
+    <NuxtLink to="/" class="home-link">
+      <img src="~/assets/images/icons/home.svg" alt="icon home" />
+    </NuxtLink>
+    <div
+      class="menu-toggle-btn"
+      :class="{ 'is-open': isNavOpen }"
+      @click="toggleNav"
+    >
+      <span class="bar top-bar"></span>
+      <span class="bar bottom-bar"></span>
+    </div>
+  </nav>
 </template>
 
 <script>

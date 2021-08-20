53a07fa3c4a55753ebd7b8adac58e57061229927
polunwu <polunwu@gmail.com>
2021-08-20 19:41:07 +0800

    加入角色百科旋轉堆疊的卡牌

--- pages/pedia.vue
+++ pages/pedia.vue
@@ -1,5 +1,23 @@
 <template>
-  <div id="page-pedia">Characters Pedia</div>
+  <div id="page-pedia">
+    <img class="top-img" src="@/assets/images/pedia/top@2x.png" alt="top" />
+    <div class="cards-container">
+      <div class="card">
+        <img
+          class="card-img show"
+          src="@/assets/images/pedia/card/01.png"
+          alt="card-img"
+        />
+      </div>
+      <div class="card">
+        <img
+          class="card-img"
+          src="@/assets/images/pedia/card/02.png"
+          alt="card-img"
+        />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>

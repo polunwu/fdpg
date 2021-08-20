757ccf09994a27d01a28c2eb89687e05c09e139f
polunwu <polunwu@gmail.com>
2021-08-20 21:01:01 +0800

    加入卡牌點擊彈出效果

---
+++ components/FlipCard.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="card">
+    <img
+      class="card-img"
+      :class="{ show: isShow }"
+      :src="require(`@/assets/images/pedia/card/${index}.png`)"
+      @click="toggle"
+      alt="card-img"
+    />
+    <div class="card-bottom-layer" v-show="isShow">
+      <div class="close-btn" @click="isShow = false">
+        <img src="@/assets/images/icons/arrow-left.svg" alt="arrow-left" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['index'],
+  data() {
+    return {
+      isShow: false,
+    }
+  },
+  methods: {
+    toggle() {
+      this.isShow = !this.isShow
+    },
+  },
+}
+</script>

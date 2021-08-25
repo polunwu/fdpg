65e8136a80137d1c56e38e59a212737a025c0a91
polunwu <polunwu@gmail.com>
2021-08-25 09:37:38 +0800

    加入 og, favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -16,10 +16,47 @@ export default {
     htmlAttrs: {
       lang: 'zh-Hant-TW',
     },
+    title: 'Fourdesire | 2021 Playground',
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      { 'http-equiv': 'X-UA-Compatible', content: 'IE=edge,chrome=1' },
+      {
+        name: 'viewport',
+        content:
+          'initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, width=device-width',
+      },
+      {
+        hid: 'description',
+        name: 'description',
+        content:
+          'Fourdesire 透由旗下生活遊戲化產品《植物保姆》、《Walkr》及《記帳城市》傳達給使用者的生活哲學， Fourdesire 乘載著 3000 萬個想讓生活變好的信念，陪伴全球以玩心養成好習慣的生活玩樂家，一起玩出更好的你！',
+      },
+      {
+        hid: 'og:title',
+        property: 'og:title',
+        content: 'Fourdesire | 2021 Playground',
+      },
+      {
+        hid: 'og:description',
+        property: 'og:description',
+        content:
+          'Fourdesire 透由旗下生活遊戲化產品《植物保姆》、《Walkr》及《記帳城市》傳達給使用者的生活哲學， Fourdesire 乘載著 3000 萬個想讓生活變好的信念，陪伴全球以玩心養成好習慣的生活玩樂家，一起玩出更好的你！',
+      },
+      {
+        hid: 'og:image',
+        property: 'og:image',
+        content: 'https://i.imgur.com/D98O6se.jpg',
+      },
+      {
+        hid: 'og:image:secure_url',
+        property: 'og:image:secure_url',
+        content: 'https://i.imgur.com/D98O6se.jpg',
+      },
+      {
+        hid: 'og:image:alt',
+        property: 'og:image:alt',
+        content: 'Fourdesire | 2021 Playground',
+      },
       { name: 'format-detection', content: 'telephone=no' },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
